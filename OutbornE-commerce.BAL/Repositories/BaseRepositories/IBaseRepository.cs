727e396109cc88acc0133627ac32789d77986e5c
Youssef Yousry <73970436+YoussefYousry@users.noreply.github.com>
2024-07-22 15:25:35 +0300

    update repositoryBase

--- OutbornE-commerce.BAL/Repositories/BaseRepositories/IBaseRepository.cs
+++ OutbornE-commerce.BAL/Repositories/BaseRepositories/IBaseRepository.cs
@@ -9,9 +9,11 @@ namespace OutbornE_commerce.BAL.Repositories.BaseRepositories
 {
 	public interface IBaseRepository<T> where T : class
 	{
-        Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> criteria, string[] includes = null);
-		Task<IEnumerable<T>> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges);
-		Task<T?> Find(Expression<Func<T, bool>> expression, bool trackChanges);
+        Task<IEnumerable<T>> FindAllAsync(string[] includes, bool withNoTracking = true);
+		//Task<IEnumerable<T>> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges);
+		Task<IEnumerable<T>> FindByCondition(Expression<Func<T, bool>> criteria, string[] includes = null);
+
+        Task<T?> Find(Expression<Func<T, bool>> expression, bool trackChanges);
 		Task<T> Create(T entity);
 		void Delete(T entity);
 		void Update(T entity);
